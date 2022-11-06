b158e24d804d4fa428cbf4bec8b05dc73ca99664
kami2049 <hamilton8186@aol.com>
2022-11-06 13:12:20 -0500

    updated files

--- action_page.php
+++ action_page.php
@@ -1,21 +1,8 @@
 <?php
 
-if(isset($_POST['submit'])) {
-    $name = $_POST['username'];
-    $mailfrom = $_POST['email'];
+$name = $_POST['username'];
+$mailfrom = $_POST['email'];
 
-}
 
+$mailheader = 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
-</head>
-<body>
-    Welcome <?php echo $_POST["username"]; ?><br>
-    Your email address is: <?php echo $_POST["email"]; ?>
-    
-</body>
-</html>
